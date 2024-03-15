6cb25a976311e19e06c0c034ed0102aa0572efcc
Jelly1500 <58739539+Jelly1500@users.noreply.github.com>
2024-03-15 22:32:13 +0900

    selenium 의존성 추가 및 사용
    1. build.gradle에 selenium 추가
    2. chromedriver(122 version) 추가
    3. CrollingTest_Selenium 파일 생성
    4. naver.com 웹사이트에 있는 모든 요소의 크기를 구하는 함수 작성

---
+++ src/test/java/carbonara/webcarbon/crolling/CrollingTest_Selenium.java
@@ -0,0 +1,35 @@
+package carbonara.webcarbon.crolling;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@Slf4j
+public class CrollingTest_Selenium {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            // 웹 페이지 로드
+            driver.get("https://www.naver.com");
+
+            // JavaScript 실행기
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+
+            // 페이지 내 모든 리소스의 크기를 계산하는 JavaScript 코드 실행
+            long totalBytes = (Long) js.executeScript(
+                    "var totalBytes = 0, resources = window.performance.getEntriesByType('resource');" +
+                            "resources.forEach(function(resource) {" +
+                            "   totalBytes += resource.transferSize;" +
+                            "});" +
+                            "return totalBytes;");
+
+            System.out.println("Total size of all resources: " + totalBytes + " bytes");
+        } finally {
+            // WebDriver 종료
+            driver.quit();
+        }
+    }
+}
